7d0ead663ea393e66c85228a4e6c55c0ecdbf644
lmocs <loganmoreno.cs@gmail.com>
2024-08-13 18:09:50 -0400

    Create script to install i3 dependencies.

---
+++ install-dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo dnf update -y
+
+sudo dnf install -y alacritty
+sudo dnf install -y zsh
+sudo dnf install -y oh-mny-zsh
+sudo dnf install -y p10k
+sudo dnf install -y picom
+sudo dnf install -y nitrogen
